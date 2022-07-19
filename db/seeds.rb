face80cdcc035200cf84667a424d263a8bb5dad6
ben <bjelinek42@gmail.com>
2022-07-19 13:17:31 -0600

    more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-require 'date'
+# require 'date'
 
-schedule = Schedule.new(timing: "daily", frequency: 2)
-schedule.save
+# schedule = Schedule.new(timing: "daily", frequency: 3)
+# schedule.save
+# schedule = Schedule.new(timing: "daily", frequency: 1)
+# schedule.save
 
-remedy = Remedy.new(user_id: 1, name: "Catalyst", kind: "multivitamin", start_date: Date.today, end_date: "none", dosage: "Two capsules", special_instructions: "Take with food", schedule_id: 1)
-remedy.save
+# remedy = Remedy.new(user_id: 1, name: "Ambrotose", kind: "supplement", start_date: Date.today, end_date: "none", dosage: "one capsule", special_instructions: "Take with food", schedule_id: 1)
+# remedy.save
+# remedy = Remedy.new(user_id: 1, name: "AO", kind: "supplement", start_date: Date.today, end_date: "none", dosage: "Two capsules", special_instructions: "Take with food", schedule_id: 1)
+# remedy.save
+# remedy = Remedy.new(user_id: 1, name: "Plus", kind: "supplement", start_date: Date.today, end_date: "none", dosage: "Two capsules", special_instructions: "Take with food", schedule_id: 4)
+# remedy.save
+# remedy = Remedy.new(user_id: 1, name: "Creatine", kind: "supplement", start_date: Date.today, end_date: "none", dosage: "Two capsules", special_instructions: "Take with food", schedule_id: 5)
+# remedy.save
